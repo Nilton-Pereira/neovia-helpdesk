ac69ccf00aad305a099d5cd639d68fb817405198
nilton-pereira <niljorgepereira@gmail.com>
2021-08-27 02:27:36 -0300

    carga inicial da base de dados

--- src/main/java/com/neovia/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/neovia/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,55 @@
 package com.neovia.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.neovia.helpdesk.domain.Chamado;
+import com.neovia.helpdesk.domain.Cliente;
+import com.neovia.helpdesk.domain.Tecnico;
+import com.neovia.helpdesk.domain.enums.Perfil;
+import com.neovia.helpdesk.domain.enums.Prioridade;
+import com.neovia.helpdesk.domain.enums.Status;
+import com.neovia.helpdesk.repositories.ChamadoRepository;
+import com.neovia.helpdesk.repositories.ClienteRepository;
+import com.neovia.helpdesk.repositories.TecnicoRepository;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
+	
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
+	
+	@Override
+	public void run(String...args) throws Exception {
+		Tecnico tec1 = new Tecnico(null,"Nilton Pereira","06722137961","niljorgepereira@gmail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null,"Andre costa","75465016795","andre.costa@neoviaengenharia.com.br", "123");
+		
+		Chamado c1 = new Chamado(null,Prioridade.MEDIA,Status.ANDAMENTO,"Chamado 01", "Primeiro chamado", tec1, cli1);
+	
+	
+	tecnicoRepository .saveAll(Arrays.asList(tec1));
+	clienteRepository.saveAll(Arrays.asList(cli1));
+	chamadoRepository .saveAll(Arrays.asList(c1));
+	
+	
+	
+	}
+	
 
 }
