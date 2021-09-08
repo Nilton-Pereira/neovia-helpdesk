7a9b4be9b301b94cc72de47b1271ab41e4526242
nilton-pereira <niljorgepereira@gmail.com>
2021-09-08 17:48:22 -0300

    repository alterados

--- src/main/java/com/neovia/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/neovia/helpdesk/repositories/PessoaRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.neovia.helpdesk.domain.Pessoa;
 
-public interface PessoaRepository extends JpaRepository< Pessoa, Integer> {
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
 }
