7499a4dcb038c58cd697eec7a8928e112cf6a254
nilton-pereira <niljorgepereira@gmail.com>
2021-10-19 01:21:57 -0300

    ENDPOINT para técnico

--- src/main/java/com/neovia/helpdesk/domain/Tecnico.java
+++ src/main/java/com/neovia/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.neovia.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.neovia.helpdesk.domain.dtos.TecnicoDTO;
 import com.neovia.helpdesk.domain.enums.Perfil;
 
 @Entity 
@@ -28,6 +30,17 @@ public class Tecnico extends Pessoa {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
 		
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/neovia/helpdesk/services/TecnicoService.java
+++ src/main/java/com/neovia/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.neovia.helpdesk.domain.Tecnico;
+import com.neovia.helpdesk.domain.dtos.TecnicoDTO;
 import com.neovia.helpdesk.repositories.TecnicoRepository;
 import com.neovia.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -24,5 +25,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
+		
 	}
 }
