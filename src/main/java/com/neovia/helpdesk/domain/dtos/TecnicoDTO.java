660570b846b3aa544c91ed318d8d828114073b44
nilton-pereira <niljorgepereira@gmail.com>
2021-10-19 03:04:00 -0300

    Validação nos Campos de criação de novo usuário

--- src/main/java/com/neovia/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/neovia/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.neovia.helpdesk.domain.Tecnico;
 import com.neovia.helpdesk.domain.enums.Perfil;
@@ -14,9 +16,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	@NotNull(message = "O campo Nome é Requirido")
 	protected String nome;
+	@NotNull(message = "O campo CPF é Requirido")
 	protected String cpf;
+	@NotNull(message = "O campo Email é Requirido")
 	protected String email;
+	@NotNull(message = "O campo Senha é Requirido")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 
