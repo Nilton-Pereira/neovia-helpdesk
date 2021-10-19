c5c9b3b0badbf120ffd57509f8068d269c9f9e63
nilton-pereira <niljorgepereira@gmail.com>
2021-10-19 01:54:08 -0300

    Excessão para cpf e email´já cadastrado

--- src/main/java/com/neovia/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/neovia/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.neovia.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.neovia.helpdesk.services.exceptions.ObjectnotFoundException;
 import com.neovia.helpdesk.services.exceptions.StandardError;
 
@@ -23,6 +24,14 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException (DataIntegrityViolationException ex, HttpServletRequest request){
+		
+		StandardError error = new StandardError(System.currentTimeMillis(),HttpStatus.BAD_REQUEST.value(),"Violação de Dados",ex.getMessage(),request.getRequestURI());
+		
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
 	
 
 }
