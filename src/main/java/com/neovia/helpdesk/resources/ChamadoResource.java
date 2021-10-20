41f5022b9ac4c8fe8247563216b3d59f1675ec52
nilton-pereira <niljorgepereira@gmail.com>
2021-10-20 18:45:42 -0300

    findAll Chamado

--- src/main/java/com/neovia/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/neovia/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.neovia.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,13 @@ public class ChamadoResource {
 	public ResponseEntity<ChamadoDTO> FindById(@PathVariable Integer id){
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll(){
+		List<Chamado> list = service.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
